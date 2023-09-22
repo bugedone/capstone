caf323e3d04e14698859765a968be19035a66121
Stu <stu@stu.nu>
2023-09-22 16:57:02 +0100

    Added version to the build and an endpoint to expose it Added k8s manifests

--- src/Capstone/Program.cs
+++ src/Capstone/Program.cs
@@ -1,6 +1,12 @@
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var version = Assembly.GetExecutingAssembly().GetName().Version;
+
 app.MapGet("/", () => "Welcome to the Capstone project!");
+
+app.MapGet("/version", () => $"Running v{version} of the Capstone app");
 
 app.Run();
